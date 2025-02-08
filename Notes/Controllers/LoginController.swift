002f2a3c269608854a26f0fc456fd3e85145e871
Elina Karimova <elinakarimova@Mac.home>
2025-02-08 18:49:00 +0100

    Added user login functionality

--- Notes/Controllers/LoginController.swift
+++ Notes/Controllers/LoginController.swift
@@ -9,6 +9,59 @@ import UIKit
 
 class LoginController: UIViewController {
     
-
+    
+    
+    // MARK: - UI Components
+    private let headerView = AuthHeaderView(title: "Sign In", subTitle: "Sign in to your account")
+    private let usernameField = CustomTextField(fieldType: .username)
+    private let passwordField = CustomTextField(fieldType: .passsword)
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    // MARK: - UI Setup
+    private func setupUI() {
+        self.view.backgroundColor = .systemBackground
+        
+        self.view.addSubview(headerView)
+        self.view.addSubview(usernameField)
+        self.view.addSubview(passwordField)
+        
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        usernameField.translatesAutoresizingMaskIntoConstraints = false
+        passwordField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.headerView.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor),
+            self.headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.headerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.headerView.heightAnchor.constraint(equalToConstant: 222),
+            
+            self.usernameField.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 12),
+            self.usernameField.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            self.usernameField.heightAnchor.constraint(equalToConstant: 55),
+            self.usernameField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            self.passwordField.topAnchor.constraint(equalTo: usernameField.bottomAnchor, constant: 22),
+            self.passwordField.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            self.passwordField.heightAnchor.constraint(equalToConstant: 55),
+            self.passwordField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85)
+        ])
+        
+        
+        
+        
+        
+        // MARK: - Selectors
         
     }
+}
---
+++ Notes/Views/CustomTextField.swift
@@ -0,0 +1,52 @@
+//
+//  CustomTextField.swift
+//  Notes
+//
+//  Created by Elina Karimova on 8/2/25.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    enum CustomTextFieldType {
+        case username
+        case email
+        case passsword
+    }
+    
+    private let authFieldType: CustomTextFieldType
+    
+    init(fieldType: CustomTextFieldType) {
+        self.authFieldType = fieldType
+        super.init(frame: .zero)
+        
+        self.backgroundColor = .secondarySystemBackground
+        self.layer.cornerRadius = 10
+        
+        self.returnKeyType = .done
+        self.autocorrectionType = .no
+        self.autocapitalizationType = .none
+        
+        self.leftViewMode = .always
+        self.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: self.frame.size.height))
+        
+        switch fieldType {
+        case .username:
+            self.placeholder = "Username"
+        case .email:
+            self.placeholder = "Email Address"
+            self.keyboardType = .emailAddress
+            self.textContentType = .emailAddress
+        case .passsword:
+            self.placeholder = "Password"
+            self.textContentType = .oneTimeCode
+            self.isSecureTextEntry = true
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
